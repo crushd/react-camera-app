de0aac18923839942569a37c2db77800f7f02900
crushd <44733457+crushd@users.noreply.github.com>
2022-02-19 07:47:24 -0800

    getVideo stream

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ function App() {
 
   const [hasPhoto, setHasPhoto] = useState(false);
 
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1920, height: 1080 },
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return <div className="App">Hello World</div>;
 }
 
