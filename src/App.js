204d62c1c4cd7007b6cf4c8c0bcab09b08d2574d
crushd <44733457+crushd@users.noreply.github.com>
2022-02-19 07:48:14 -0800

    takePhoto

--- src/App.js
+++ src/App.js
@@ -21,6 +21,21 @@ function App() {
       });
   };
 
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16 / 9);
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext("2d");
+    ctx.drawImage(video, 0, 0, width, height);
+    setHasPhoto(true);
+  };
+
   useEffect(() => {
     getVideo();
   }, [videoRef]);
