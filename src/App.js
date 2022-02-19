92795dc25fa8238b7e28c5760a7b987d3e8bec60
crushd <44733457+crushd@users.noreply.github.com>
2022-02-19 07:47:00 -0800

    set up react hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import "./App.css";
+import React, { useRef, useEffect, useState } from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+
+  const [hasPhoto, setHasPhoto] = useState(false);
+
   return <div className="App">Hello World</div>;
 }
 
